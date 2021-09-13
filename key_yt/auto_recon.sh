147459ec47bd22d26d89b8dea73222a4cc91e43a
zzzt27 <paskal45273@gmail.com>
2021-09-13 18:18:49 +0700

    Update auto_recon.sh

--- key_yt/auto_recon.sh
+++ key_yt/auto_recon.sh
@@ -11,8 +11,7 @@ fi
 
 SERVICE_NAME="Auto Reconnect"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-#INTERVAL="1"
-#HOST="bing.com"
+TUNNEL_MODE="$(grep 'mode":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
 
 function loop() {
 n=0
@@ -47,11 +46,70 @@ done
 
 #stop libernet
 recon(){
-    curl -d '{"action":"restart_libernet"}' -H "Content-Type: application/json" -X POST http://192.168.1.1/libernet/api.php
+    #curl -d '{"action":"restart_libernet"}' -H "Content-Type: application/json" -X POST http://192.168.1.1/libernet/api.php
+	stop_services
+	sleep 2
+	start_services
+}
+
+function start_services() {
+  # write to service log
+  #"${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start Tunnel Service"
+  case "${TUNNEL_MODE}" in
+    "0")
+	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start SSH Service"
+      "${LIBERNET_DIR}/bin/ssh.sh" -r
+      ;;
+    "1")
+	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start v2ray Service"
+      "${LIBERNET_DIR}/bin/v2ray.sh" -r
+      ;;
+    "2")
+	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start SSH-SSL Service"
+      "${LIBERNET_DIR}/bin/ssh-ssl.sh" -r
+      ;;
+    "3")
+	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start Trojan Service"
+      "${LIBERNET_DIR}/bin/trojan.sh" -r
+      ;;
+    "4")
+	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start shadowsocks Service"
+      "${LIBERNET_DIR}/bin/shadowsocks.sh" -r
+      ;;
+  esac
+  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start Tun2Socks"
+  "${LIBERNET_DIR}/bin/tun2socks.sh" -v
+  sleep 5
+  "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: blue\">[AUTO-RC]Checking...</span>"
+}
+
+function stop_services() {
+  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Stopping Tunnel Service"
+  case "${TUNNEL_MODE}" in
+    "0")
+      "${LIBERNET_DIR}/bin/ssh.sh" -s
+      ;;
+    "1")
+      "${LIBERNET_DIR}/bin/v2ray.sh" -s
+      ;;
+    "2")
+      "${LIBERNET_DIR}/bin/ssh-ssl.sh" -s
+      ;;
+    "3")
+      "${LIBERNET_DIR}/bin/trojan.sh" -s
+      ;;
+    "4")
+      "${LIBERNET_DIR}/bin/shadowsocks.sh" -s
+      ;;
+    "5")
+      "${LIBERNET_DIR}/bin/openvpn.sh" -s
+      ;;
+  esac
+  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Stopping Tun2Socks"
+  "${LIBERNET_DIR}/bin/tun2socks.sh" -w
 }
 
 function run() {
-  # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
   echo -e "Starting ${SERVICE_NAME} service ..."
   screen -AmdS auto-recon "${LIBERNET_DIR}/bin/auto_recon.sh" -l \
@@ -62,6 +120,8 @@ function stop() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
+  # disableb led
+  #hg680p.sh -lan dis &>/dev/null
   kill $(screen -list | grep auto-recon | awk -F '[.]' {'print $1'}) > /dev/null 2>&1
   echo -e "${SERVICE_NAME} service stopped!"
 }
